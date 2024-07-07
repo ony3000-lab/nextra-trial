3731abf2b0f37a383fae925cd71ae24c3eaae868
Hyeonjong <nianelo4@gmail.com>
2024-07-07 17:26:08 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-pretendard)', ...fontFamily.sans],
+      },
+      screens: {
+        'non-touchscreen': { raw: '(hover: hover) and (pointer: fine)' },
+      },
+    },
+  },
+  plugins: [],
+};
